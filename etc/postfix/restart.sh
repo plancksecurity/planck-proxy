b748f6a5913a2f6f80e8e8b09e269334c0684724
Mailhub <root@hub.pep.security>
2022-12-14 10:50:05 +0100

    Outsource Postfix/SpamAssassin/OpenDKIM configs into own repo (https://git.pep.security/pep/mailhub)

--- etc/postfix/restart.sh
+++
@@ -1,7 +0,0 @@
-newaliases
-postalias /etc/aliases
-postmap /etc/postfix/ip.blacklist
-postmap /etc/postfix/transport
-postmap /etc/postfix/virtual
-postmap /etc/postfix/header_checks
-/etc/init.d/postfix restart
