d378d066c92cd64be9a6c1419e91338d28f38ce2
David Beltra <dbe@pep.security>
2023-01-26 12:54:26 +0100

    test get contact info

--- tests/unit/test_get_contact_info.py
+++ tests/unit/test_get_contact_info.py
@@ -1,16 +1,21 @@
 import pytest
-import glob
+
+from helpers import collect_email
 from pEphelpers import get_contact_info
 
 
-@pytest.mark.parametrize("test_email, expected",
+@pytest.mark.parametrize("email_finder_expr, expected",
     [
-        ("01", ("andy@pep.security", "andy@0x3d.lu")),
-        ("02", ("andy@0x3d.lu", "aw@gate.pep.security")),
-        # ("06", ("support@pep-security.net", ["aw@pep.security", "foo@0x3d.lu", "bla@0x3d.lu"])), TODO: This fails!
+        ("01*", ("andy@pep.security", "andy@0x3d.lu")),
+        ("02*", ("andy@0x3d.lu", "aw@gate.pep.security")),
+        ("11*", ("service@pep-security.net", "andy@pep-security.net")),
     ])
-def test_get_contact_pass(test_email, expected):
-    email = glob.glob('emails/' + test_email + '*')[0]
-    with open(email) as f:
-        assert get_contact_info(f.read()) == expected
+def test_get_contact_pass(email_finder_expr, expected):
+    assert get_contact_info(collect_email(email_finder_expr)) == expected
+
+def test_get_contact_fail():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+            get_contact_info(collect_email("06*"))
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 3
 
