430f2fa56b806dc91741fc0d6d449b0794c73ea5
David Beltra <dbe@pep.security>
2023-01-26 12:53:42 +0100

    use env to locate the tests main folder

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import sys
+import os
 # Adding the path to the root folder so the files can be imported in the tests
 ROOT_PATH = Path(__file__).parent.parent.parent
 sys.path.insert(0, str(ROOT_PATH))
+
+os.environ['TEST_ROOT'] = str(Path(__file__).parent.parent)
