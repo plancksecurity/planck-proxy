fa036c6c3a4dbaca9fd67c12ca281c373d5a47cb
David Beltra <dbe@pep.security>
2023-01-31 11:28:49 +0100

    test refactor

--- tests/unit/test_disable_encryption.py
+++ tests/unit/test_disable_encryption.py
@@ -6,43 +6,38 @@ from pEphelpers import get_contact_info
 
 
 @pytest.mark.parametrize('collect_email', ["basic_noencrypt.eml"], indirect=True)
-def test_encrypt_message(collect_email):
+def test_encrypt_noencrypt_message(test_dirs, collect_email, extra_keypair):
     cmd_env = os.environ.copy()
-    work_dir = 'tests/work'
-    keys_dir = 'tests/keys'
-    cmd_env['work_dir'] = work_dir
-    cmd_env['keys_dir'] = keys_dir
+    cmd_env['work_dir'] = test_dirs['work']
+    cmd_env['keys_dir'] = test_dirs['keys']
+    cmd_env['EXTRA_KEY'] = extra_keypair.fpr
     test_email_from, test_email_to = get_contact_info(collect_email)
-    with open('tests/emails/basic_noencrypt.eml', 'rb') as email:
+    with open(test_dirs['emails'] / 'basic_noencrypt.eml', 'rb') as email:
         subprocess.run(['./pEpgate encrypt --DEBUG'], shell=True,
             capture_output=True, input=email.read(), env=cmd_env)
 
-    decrypt_out_path = work_dir + '/' + test_email_from + '/' + test_email_to
+    decrypt_out_path = test_dirs['work'] / test_email_from / test_email_to
     out_folder = [f.path for f in os.scandir(decrypt_out_path)][0]
     encrypted = out_folder + '/in.encrypt.processed.eml'
     with open(encrypted) as encrypted_email:
         encrypted_data = encrypted_email.read()
     assert "Hello proxy" in encrypted_data
     assert "NOENCRYPT" not in encrypted_data
-    shutil.rmtree(work_dir, ignore_errors=True)
 
-@pytest.mark.xfail(reason="bob's key needs to be in the keys dir")
 @pytest.mark.parametrize('collect_email', ["basic_noencrypt.eml"], indirect=True)
-def test_encrypt_message_no_debug(collect_email):
+def test_encrypt_noencrypt_message_no_debug(test_dirs, collect_email, extra_keypair, bob_key):
     cmd_env = os.environ.copy()
-    work_dir = 'tests/work'
-    keys_dir = 'tests/keys'
-    cmd_env['work_dir'] = work_dir
-    cmd_env['keys_dir'] = keys_dir
+    cmd_env['work_dir'] = test_dirs['work']
+    cmd_env['keys_dir'] = test_dirs['keys']
+    cmd_env['EXTRA_KEY'] = extra_keypair.fpr
     test_email_from, test_email_to = get_contact_info(collect_email)
-    with open('tests/emails/basic_noencrypt.eml', 'rb') as email:
+    with open(test_dirs['emails'] / 'basic_noencrypt.eml', 'rb') as email:
         subprocess.run(['./pEpgate encrypt'], shell=True,
             capture_output=True, input=email.read(), env=cmd_env)
 
-    decrypt_out_path = work_dir + '/' + test_email_from + '/' + test_email_to
+    decrypt_out_path = test_dirs['work'] / test_email_from / test_email_to
     out_folder = [f.path for f in os.scandir(decrypt_out_path)][0]
     encrypted = out_folder + '/in.encrypt.processed.eml'
     with open(encrypted) as encrypted_email:
         encrypted_data = encrypted_email.read()
     assert "Hello proxy" not in encrypted_data
-    # shutil.rmtree(work_dir, ignore_errors=True)
