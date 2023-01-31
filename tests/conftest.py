86558c4f499c0fef24811d425d5a65c30b0cad1d
David Beltra <dbe@pep.security>
2023-01-31 11:28:31 +0100

    parametrize keys in tests and do some test refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,26 @@ import os
 import glob
 import random
 import string
-import pathlib
 import pytest
+import shutil
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class Key:
+    name: str
+    address: str
+    fpr: str
+    def get_public(self):
+        return self.fpr + '.pub.asc'
+    def get_private(self):
+        return self.fpr + '.sec.asc'
+
+
+
+EXTRA_KEY = Key('extra', 'proxy@test.com', "3F8B5F3DA55B39F1DF6DE37B6E9B9F4A3035FCE3")
+BOB_KEY = Key('bob', 'bob@pep.security', "CC47DB45FDAF07712F1D9F5BFE0D6DE1B8C05AE8")
 
 @pytest.hookimpl(trylast=True)  # run after configure for TempPathFactory
 def pytest_configure(config):
@@ -19,8 +37,17 @@ def pytest_configure(config):
 def per_user_directory():
     # BUG: This assumes Engine internals
     # TODO: Fix: Does not work for Windows
-    return pathlib.Path(os.environ["HOME"]) / ".pEp"
+    return Path(os.environ["HOME"]) / ".pEp"
 
+@pytest.fixture
+def test_dirs(tmp_path):
+    return {
+        'root': Path(os.environ['TEST_ROOT']),
+        'keys': tmp_path / "keys",
+        'test_keys': Path(os.environ['TEST_ROOT']) / "test_keys",
+        'work': tmp_path / "work",
+        'emails': Path(os.environ['TEST_ROOT']) / "emails",
+    }
 
 @pytest.fixture
 def reset_pep_folder(tmp_path):
@@ -32,11 +59,27 @@ def reset_pep_folder(tmp_path):
     pep_folder.mkdir(parents=True)
     return pep_folder
 
-KEYS_DIR = pathlib.Path(__file__).parent.absolute() / "keys"
+@pytest.fixture
+def extra_keypair(test_dirs):
+    pubkey = test_dirs['test_keys'] / EXTRA_KEY.get_public()
+    privkey = test_dirs['test_keys'] / EXTRA_KEY.get_private()
+
+    if not os.path.exists(test_dirs['keys']):
+        os.makedirs(test_dirs['keys'])
+
+    shutil.copy(pubkey, test_dirs['keys'])
+    shutil.copy(privkey, test_dirs['keys'])
+    return EXTRA_KEY
 
 @pytest.fixture
-def keys_dir():
-    return KEYS_DIR
+def bob_key(test_dirs):
+    pubkey = test_dirs['test_keys'] / BOB_KEY.get_public()
+
+    if not os.path.exists(test_dirs['keys']):
+        os.makedirs(test_dirs['keys'])
+
+    shutil.copy(pubkey, test_dirs['keys'])
+    return BOB_KEY
 
 @pytest.fixture
 def mailbot_address():
--- tests/unit/test_send.py
+++ tests/unit/test_send.py
@@ -3,15 +3,8 @@ import pytest
 import sys
 
 
+@pytest.mark.skipif(sys.platform  == 'darwin', reason="No server in a darwin system, so sendmail will fail")
 @pytest.mark.parametrize('collect_email', ["test_send_bot.eml"], indirect=True)
 def test_send(collect_email, mailbot_address):
     test_msg = collect_email.replace('[[TO_ADDR]]', mailbot_address)
-    if sys.platform == 'darwin':
-    # No server in a darwin system, so sendmail will fail
-        with pytest.raises(SystemExit) as pytest_wrapped_e:
-            res = sendmail(test_msg)
-        assert pytest_wrapped_e.type == SystemExit
-        assert pytest_wrapped_e.value.code == 6
-    else:
-        res = sendmail(test_msg)
-
+    res = sendmail(test_msg)
