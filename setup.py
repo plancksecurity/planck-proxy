52dff92ac0d3e938e371983b763cf72524f75368
David Beltra <dbe@pep.security>
2023-02-07 15:23:20 +0100

    Try packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    # keep aligned with setup.cfg
+    setup_requires = [
+        "setuptools >= 39.2.0",
+        "setuptools_scm >= 4.1.2",
+        "wheel",
+    ],
+    use_scm_version = {
+        'write_to': "pEpGate/__version__.py"
+    },
+    entry_points = { 'console_scripts': 'pepgate' },
+)
