fc077427c67e6aed28c158764ebb8a7b6a124bd6
David Beltra <dbe@pep.security>
2023-02-23 09:41:56 +0100

    relocate argparser so main can be used as a call

--- src/pEpgate.py
+++ src/pEpgate.py
@@ -7,30 +7,8 @@ from pEphelpers import get_default, cleanup
 from pEpgatesettings import settings, init_settings
 from pEpgatemain import *
 
-def main():
-	init_settings()
+def main(args):
 	atexit.register(cleanup)
-
-	parser = argparse.ArgumentParser(description='pEp Proxy CLI.')
-	parser.add_argument('mode', choices=["encrypt", "decrypt"], help='Mode')
-	parser.add_argument('--DEBUG', action='store_true',
-		default=get_default("DEBUG", type=bool), help=f'Set DEBUG mode, default is {get_default("DEBUG")}')
-	parser.add_argument('--EXTRA_KEYS', nargs='*', default=get_default("EXTRA_KEYS", type=list),
-		help=f'Space-separated fingerprint(s) to use as extra key(s) when encrypting messages, default is "{get_default("EXTRA_KEYS")}"')
-	parser.add_argument('--keys_dir', default=get_default("keys_dir"),
-		help=f'Directory where the extra key should be imported from, default is "{get_default("keys_dir")}"')
-	parser.add_argument('--work_dir', default=get_default("work_dir"),
-		help=f'Directory where the command outputs are placed, default is "{get_default("work_dir")}"')
-	parser.add_argument('--SMTP_HOST', default=get_default("SMTP_HOST"),
-		help=f'Address of the SMTP host used to send the messages. Default "{get_default("SMTP_HOST")}"')
-	parser.add_argument('--SMTP_PORT', type=int, default=get_default("SMTP_PORT"),
-		help=f'Port of the SMTP host used to send the messages. Default "{get_default("SMTP_PORT")}"')
-
-	args = parser.parse_args()
-
-	for key,val in vars(args).items():
-		settings[key] = val
-
 
 	msg = {}
 	us = {}
@@ -63,4 +41,25 @@ def main():
 	log_session()
 
 if __name__ == '__main__':
-	main()
+	init_settings()
+
+	parser = argparse.ArgumentParser(description='pEp Proxy CLI.')
+	parser.add_argument('mode', choices=["encrypt", "decrypt"], help='Mode')
+	parser.add_argument('--DEBUG', action='store_true',
+		default=get_default("DEBUG", type=bool), help=f'Set DEBUG mode, default is {get_default("DEBUG")}')
+	parser.add_argument('--EXTRA_KEYS', nargs='*', default=get_default("EXTRA_KEYS", type=list),
+		help=f'Space-separated fingerprint(s) to use as extra key(s) when encrypting messages, default is "{get_default("EXTRA_KEYS")}"')
+	parser.add_argument('--keys_dir', default=get_default("keys_dir"),
+		help=f'Directory where the extra key should be imported from, default is "{get_default("keys_dir")}"')
+	parser.add_argument('--work_dir', default=get_default("work_dir"),
+		help=f'Directory where the command outputs are placed, default is "{get_default("work_dir")}"')
+	parser.add_argument('--SMTP_HOST', default=get_default("SMTP_HOST"),
+		help=f'Address of the SMTP host used to send the messages. Default "{get_default("SMTP_HOST")}"')
+	parser.add_argument('--SMTP_PORT', type=int, default=get_default("SMTP_PORT"),
+		help=f'Port of the SMTP host used to send the messages. Default "{get_default("SMTP_PORT")}"')
+
+	args = parser.parse_args()
+	for key,val in vars(args).items():
+		settings[key] = val
+
+	main(args)
