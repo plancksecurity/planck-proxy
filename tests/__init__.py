3363fd226bb2c468aa2f754d36ab5f0e4f8ee170
David Beltra <dbe@pep.security>
2023-02-24 13:26:31 +0100

    cleanup

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,8 +6,7 @@ import os
 TESTS_ROOT = Path(os.path.dirname(__file__))
 PROJECT_ROOT_PATH = TESTS_ROOT.parent
 MAIN_PROJECT_FILES_PATH = PROJECT_ROOT_PATH / 'src'
-# sys.path.insert(0, str(PROJECT_ROOT_PATH))
 sys.path.insert(0, str(MAIN_PROJECT_FILES_PATH))
 
-os.environ['TEST_ROOT'] = str(Path(TESTS_ROOT))
+os.environ['TEST_ROOT'] = str(TESTS_ROOT)
 os.environ['PROJECT_ROOT'] = str(PROJECT_ROOT_PATH)
