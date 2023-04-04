61f15af2438cc2d9fa6224cf631400badf31c147
David Beltra <dbe@pep.security>
2023-04-04 11:35:02 +0200

    improve encrypt script

--- src/scripts/encrypt.py
+++ src/scripts/encrypt.py
@@ -1,17 +1,20 @@
 import shutil
 import argparse
 import os
+import re
+import email
+import sqlite3
 
 
-def encrypt_msg(msg, dest_key, extra_key, extra_key_fpr, home_dir, debug):
+def encrypt_msg(msg, our_key, dest_key, extra_key, home_dir, debug):
     """
     Encrypts an email message using p≡p.
 
     Args:
         msg (str): Path to the email to encrypt.
+        our_key (str): Path to the public key of the message sender.
         dest_key (str): Path to the public key of the message recipient.
         extra_key (str): Path to the public extra key.
-        extra_key_fpr (str): Fingerprint of the extra key.
         home_dir (str): Location of the home folder.
         debug (bool): Keep the home folder and output debug info.
 
@@ -20,36 +23,100 @@ def encrypt_msg(msg, dest_key, extra_key, extra_key_fpr, home_dir, debug):
     """
     # Change to pEp home
     home = os.environ.get('HOME')
+    if os.path.isdir(home_dir):
+        shutil.rmtree(home_dir)
+        if debug:
+            print(f'previous p≡p directory at {home_dir} erased.')
     if not os.path.isdir(home_dir):
         if debug:
             print(f'p≡p directory created at {home_dir}')
         os.makedirs(home_dir)
 
     os.environ['HOME'] = home_dir
+
     import pEp
 
-    if dest_key:
-        with open(dest_key, 'r') as f:
-            key_data = f.read()
-        pEp.import_key(key_data)
-        if debug:
-            print(f'recipient key at {dest_key} successfuly imported')
-
     if extra_key:
+        if debug:
+            print('=================')
+            print('Extra key')
+            print('=================')
+
         with open(extra_key, 'r') as f:
             key_data = f.read()
         pEp.import_key(key_data)
+
         if debug:
             print(f'extra_key at {extra_key} successfuly imported')
+
+        keys_db_path = os.path.join(os.environ['HOME'], '.pEp', 'keys.db')
+        db = sqlite3.connect(keys_db_path)
+        keys = db.execute("SELECT primary_key FROM keys")
+        imported_keys = [key[0] for key in keys]
+        extra_key_fpr = imported_keys[0]
+
+        if debug:
+            print(f'extracted extra_key fingerprint {extra_key_fpr} from the database')
+
+    with open(dest_key, 'r') as f:
+        key_data = f.read()
+    pEp.import_key(key_data)
+    if debug:
+        print('=================')
+        print('Recipient key')
+        print('=================')
+        print(f'recipient key at {dest_key} successfuly imported')
+
 
     # Encrypt message
     with open(msg, 'r') as f:
         msg = f.read()
     if debug:
         print('=================')
-        print('Plain message')
+        print('Plain message opened')
         print('=================')
         print(msg)
+
+
+    if debug:
+        print('==========================')
+        print('Set our own identity')
+        print('==========================')
+
+
+    mailparseregexes = [
+        r"<([\w\-\_\"\.]+@[\w\-\_\"\.{1,}]+)>",
+        r"<?([\w\-\_\"\.]+@[\w\-\_\"\.{1,}]+)>?"
+    ]
+
+    msgfrom = ""
+    mail_obj = email.message_from_string(msg)
+    from_headers = mail_obj.get_all("From")
+
+    for mpr in mailparseregexes:
+        msgfrom = "-".join(from_headers)
+        msgfrom = "-".join(re.findall(re.compile(mpr), msgfrom))
+        if len(msgfrom) > 0:
+            break
+
+    msgfrom = msgfrom.lower()
+    if debug:
+        print(f'found our address in the message headers {msgfrom}')
+
+    match = re.match(r'^([^@]+)', msgfrom)
+    username = match.group(1)
+
+    with open(our_key, 'r') as f:
+        key_data = f.read()
+    i = pEp.import_key(key_data)
+    our_key_fpr = i[0].fpr
+
+    if debug:
+        print(f'our_key at {our_key} successfuly imported with fpr {our_key_fpr}')
+
+    i = pEp.Identity(msgfrom, username)
+    pEp.myself(i)
+
 
     msg = pEp.Message(msg)
     if extra_key:
@@ -71,18 +138,15 @@ def encrypt_msg(msg, dest_key, extra_key, extra_key_fpr, home_dir, debug):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Encrypts an email message using p≡p")
     parser.add_argument('msg', help='Path to the email to encrypt')
-    parser.add_argument('--d', '--dest_key', default=None,
-                        help='pub key of the message recipient')
-    parser.add_argument(
-        '--e', '--extra_key', default='../../tests/test_keys/3F8B5F3DA55B39F1DF6DE37B6E9B9F4A3035FCE3.pub.asc', help='pub extra key')
-    parser.add_argument(
-        '--f', '--fpr', default='3F8B5F3DA55B39F1DF6DE37B6E9B9F4A3035FCE3', help='fpr of the extra key')
+    parser.add_argument('our_key', help='path to the private key of the message sender')
+    parser.add_argument('dest_key', help='path to the pub key of the message recipient')
+    parser.add_argument('--e', '--extra_key', default=None, help='path to the public extra key')
     parser.add_argument('--m', '--home_dir', default='tmp_home',
-                        help='Location of the home folder')
+                        help='Location of the temporary home folder')
     parser.add_argument('--debug', action='store_true',
                         help='Keep the home folder and output debug info')
 
     args = parser.parse_args()
-    encrypt_msg(args.msg, args.d, args.e, args.f, args.m, args.debug)
+    encrypt_msg(args.msg, args.our_key, args.dest_key, args.e, args.m, args.debug)
