ddef90c8dcdf57513b3ac9d1a29f72c143897e15
David Beltra <dbe@pep.security>
2023-05-16 14:42:28 +0200

    extra dts test cases

--- tests/unit/test_dts.py
+++ tests/unit/test_dts.py
@@ -17,3 +17,31 @@ def test_dts(set_settings, collect_email):
     enable_dts(message)
 
     assert settings["dts"] == "dtsaddress@icandts.org"
+
+
+@pytest.mark.parametrize("collect_email", ["basic.eml"], indirect=True)
+def test_dts_no_header(set_settings, collect_email):
+    global settings
+    settings["mode"] = "decrypt"
+    settings["dts_domains"] = ["icandts.org"]
+    settings["work_dir"] = "work"
+
+    message = Message()
+    message.inmail = collect_email.decode()
+    enable_dts(message)
+
+    assert settings["dts"] is None
+
+
+@pytest.mark.parametrize("collect_email", ["basic_dts.eml"], indirect=True)
+def test_dts_unallowed_domain(set_settings, collect_email):
+    global settings
+    settings["mode"] = "decrypt"
+    settings["dts_domains"] = ["another_dts_domain.org"]
+    settings["work_dir"] = "work"
+
+    message = Message()
+    message.inmail = collect_email.decode()
+    enable_dts(message)
+
+    assert settings["dts"] is None
