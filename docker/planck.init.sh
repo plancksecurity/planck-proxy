6952a827332ebae6d4acdd85c744d3a267b66448
proxymxdev.planck.dev <git@proxymxdev.planck.dev>
2023-09-28 14:34:42 +0200

    Add /volume.skel straight into the image (use as source in env2config.py to populate a potentially empty volume), nexthopip -> nexthop (for M365 we need to use multi-A-record hostnames)

--- docker/planck.init.sh
+++ docker/planck.init.sh
@@ -7,11 +7,13 @@ rsyslogd
 echo -e "\n\n=== $(date) Planck Proxy started ===\n" >> /volume/debug.log
 ln -s /volume/debug.log /home/proxy/debug.log
 
-# Generate config files from templates in /docker/volume/
+# Generate config files from templates in /volume/
+cp -pravin /volume.skel/* /volume/
 /env2config.py || true
 chown proxy:proxy /home/proxy /volume/debug.log -R
 
-# TODO: OpenDKIM + keys?
+# TODO?: Symlink /var/spool/postfix into /volume (to retain undeliverable mails across container restarts)
+# TODO?: OpenDKIM + keys
 
 # Generate lookup tables for Postfix
 newaliases
