4298eb1683069327c9e8062cf4522030351c71fe
Your Name <you@example.com>
2023-11-02 10:57:41 +0100

    Integration Test + Email

--- tests/integration/test_decrypt.py
+++ tests/integration/test_decrypt.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import pytest
+from pathlib import Path
 from proxy.utils.parsers import get_contact_info
 from compare_mails import get_email_body
 from override_settings import override_settings
@@ -62,3 +63,26 @@ def test_decrypt_message(
     with open(decrypted) as decrypted_email:
         decrypted_data = decrypted_email.read()
     assert "Hello encrypted world!" in decrypted_data
+
+@pytest.mark.parametrize("collect_email", ["basic_no_delivered-to.eml"], indirect=True)
+def test_decrypt_message_no_delivered_to(
+    set_settings,
+    settings_file,
+    test_dirs,
+    collect_email,
+    extra_keypair,
+    test_settings_dict,
+):
+    email = collect_email.decode()
+    test_email_from, test_email_to = get_contact_info(email)
+    settings_file = override_settings(test_dirs, settings_file, test_settings_dict)
+
+    command = f"planckproxy decrypt {settings_file} --recipients {test_email_from}"
+    p = subprocess.run([command], shell=True, capture_output=True, input=collect_email)
+
+    assert p.stderr == b""
+    assert p.returncode == 0
+
+    sender_folder = test_dirs["work"] / test_email_to / test_email_from
+
+    assert Path(sender_folder).exists
