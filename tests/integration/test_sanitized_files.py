bed47b29f52443819cd2890b9b4c697496ee56b0
Alice <am@planck.security>
2023-11-14 11:19:07 +0100

    Fixed Test Address

--- tests/integration/test_sanitized_files.py
+++ tests/integration/test_sanitized_files.py
@@ -6,7 +6,7 @@ from proxy.utils.sanitizer import sanitize_email_address
 from override_settings import override_settings
 
 
-@pytest.mark.parametrize("collect_email", ["basic.enc.eml"], indirect=True)
+@pytest.mark.parametrize("collect_email", ["basic_unsanitized.eml"], indirect=True)
 def test_sanitized_files(
     set_settings,
     settings_file,
@@ -17,13 +17,13 @@ def test_sanitized_files(
 ):
     email = collect_email.decode()
     test_email_from, test_email_to = get_contact_info(email)
-    test_email_from = 'alice"test"@planck.secuirty'
     settings_file = override_settings(test_dirs, settings_file, test_settings_dict)
 
     command = f"planckproxy decrypt {settings_file}"
     p = subprocess.run([command], shell=True, capture_output=True, input=collect_email)
     assert p.stderr == b""
     assert p.returncode == 0
+    
 
     decrypt_out_path = test_dirs["work"] / sanitize_email_address(test_email_to) / sanitize_email_address(test_email_from)
     assert os.listdir(decrypt_out_path)
