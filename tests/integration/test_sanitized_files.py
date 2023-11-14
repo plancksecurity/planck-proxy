6444d2f33759c9a35735ee2a47b294a28fd806c2
Alice <am@planck.security>
2023-11-14 11:04:50 +0100

    Implemented adequate test address

--- tests/integration/test_sanitized_files.py
+++ tests/integration/test_sanitized_files.py
@@ -17,6 +17,7 @@ def test_sanitized_files(
 ):
     email = collect_email.decode()
     test_email_from, test_email_to = get_contact_info(email)
+    test_email_from = 'alice"test"@planck.secuirty'
     settings_file = override_settings(test_dirs, settings_file, test_settings_dict)
 
     command = f"planckproxy decrypt {settings_file}"
