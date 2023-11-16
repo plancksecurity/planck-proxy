2bc61cf9f795ede8392aa197cfa3f090068f9983
David Beltra <dbe@pep.security>
2023-11-16 15:54:02 +0100

    separate export logleven from command loglevel

--- src/proxy/utils/printers.py
+++ src/proxy/utils/printers.py
@@ -146,7 +146,7 @@ def dbg(text, printtiming=False, pub=True, log_level="DEBUG"):
     text = str(text) + (" " + c("{:1.6f}".format(took) + "s", 5) if printtiming else "")
     ts_text = c(thisactiontime.strftime("%d.%m.%Y %H:%M:%S.%f"), 3)  + " - " + log_level + " - " + text
 
-    if pub is True and (get_numeric_log_level(log_level) >= console_logger.getEffectiveLevel()):
+    if pub is True and (get_numeric_log_level(log_level) >= get_numeric_log_level(settings["export_log_level"])):
         settings["adminlog"] += toplain(ts_text) + "\n"
         settings["textlog"] += toplain(ts_text) + "\n"
         settings["htmllog"] += tohtml(ts_text) + "<br>\n"
