c1b421e67b4bc9f527e5fa6443b3c58baba97d27
David Beltra <dbe@pep.security>
2023-11-21 12:45:23 +0100

    add access token authentication

--- docker.build.sh
+++ docker.build.sh
@@ -1,1 +1,1 @@
-docker build --tag=dockerreg.planck.security/planckproxy $* .
+docker build --build-arg gh_user=${GH_USER} --build-arg gh_token=${GH_TOKEN} --tag=dockerreg.planck.security/planckproxy $* .
