d41b91ef59a0b3cb5712e7b6c0f560d597b6706f
David Beltra <dbe@pep.security>
2023-11-21 12:49:26 +0100

    capitalize env vars on build

--- docker.build.sh
+++ docker.build.sh
@@ -1,1 +1,1 @@
-docker build --build-arg gh_user=${GH_USER} --build-arg gh_token=${GH_TOKEN} --tag=dockerreg.planck.security/planckproxy $* .
+docker build --build-arg GH_USER=${GH_USER} --build-arg GH_TOKEN=${GH_TOKEN} --tag=dockerreg.planck.security/planckproxy $* .
